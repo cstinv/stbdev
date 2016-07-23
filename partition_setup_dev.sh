46ba722c0785d9ac0ccaac93d65d2686f65def08
Claus Still <claus.still@gmail.com>
2016-07-23 10:17:49 +0300

    Copying apply_noobs_os_config file to home directory

--- partition_setup_dev.sh
+++ partition_setup_dev.sh
@@ -2,15 +2,16 @@
 
 set -ex
 
-if [ -z "$part1" ] || [ -z "$part2" ]; then
-  printf "Error: missing environment variable part1 or part2\n" 1>&2
+if [ -z "$part1" ] || [ -z "$part2" ] || [ -z "$part3" ]; then
+  printf "Error: missing environment variable part1 or part2 or part3\n" 1>&2
   exit 1
 fi
 
-mkdir -p /tmp/1 /tmp/2
+mkdir -p /tmp/1 /tmp/2 /tmp/3
 
 mount "$part1" /tmp/1
 mount "$part2" /tmp/2
+mount "$part3" /tmp/3
 
 sed /tmp/1/cmdline.txt -i -e "s|root=/dev/[^ ]*|root=${part2}|"
 sed /tmp/2/etc/fstab -i -e "s|^.* / |${part2}  / |"
@@ -20,8 +21,11 @@ if ! grep -q resize /proc/cmdline; then
   sed -i 's/ quiet init=.*$//' /tmp/1/cmdline.txt
 fi
 
+cp /tmp/2/etc/init.d/apply* /tmp/3
+
 
 umount /tmp/1
 umount /tmp/2
+umount /tmp/3
 
 
