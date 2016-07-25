7aa7331ef6eda84f6734d788dbd796e37c017b11
Claus Still <claus.still@gmail.com>
2016-07-25 23:47:33 +0300

    Changed execution rights on scripts

--- init_stb/init_stb_config.sh
+++ init_stb/init_stb_config.sh
@@ -6,12 +6,14 @@ cd /etc/init.d
 # Init OS settings
 log_daemon_msg "STBDEV: Initializing OS settings for IOT STB"
 sudo wget https://github.com/cstinv/stbdev/raw/master/init_stb/init_os_settings.sh
-/etc/init.d/init_os_settings.sh
+chmod 774 init_os_settings.sh
+. /etc/init.d/init_os_settings.sh
 
 # Init home directory on separate partition
 log_daemon_msg "STBDEV: Initializing home directory on separate partition"
 sudo wget https://github.com/cstinv/stbdev/raw/master/init_stb/init_home_directory.sh
-/etc/init.d/init_home_directory.sh
+chmod 774 init_home_directory.sh
+. /etc/init.d/init_home_directory.sh
 
 
 
