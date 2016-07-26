a87598649867a6ac6d347e0bee31e61e5153edc7
Claus Still <claus.still@gmail.com>
2016-07-26 10:11:41 +0300

    Will use grep to find current home directory mount point

--- init_stb/init_home_directory.sh
+++ init_stb/init_home_directory.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 # Move home directory to separate data partition
 
+homedir=$(df -h | grep -o "/media/pi/home.*")
+log_daemon_msg "STBDEV: home directory = $homedir"
 cd /home
-sudo cp -rp ./ /media/pi/home
-sudo umount /media/pi/home
-sudo rm -rf /media/pi/home
+sudo cp -rp ./ $homedir
+sudo umount $homedir
+sudo rm -rf $homedir
 sudo mv /home /old_home
-sudo mkdir home
+sudo mkdir /home
 sudo chmod 666 /etc/fstab
 sudo echo "/dev/mmcblk0p8 /home ext4 defaults,noatime,nodiratime 0 0" >> /etc/fstab
 sudo chmod 444 /etc/fstab
