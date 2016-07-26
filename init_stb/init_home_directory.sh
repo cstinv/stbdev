ebcba6dff37925690b7b961825af362220f617f7
Claus Still <claus.still@gmail.com>
2016-07-26 10:47:04 +0300

    Mounting fix in script

--- init_stb/init_home_directory.sh
+++ init_stb/init_home_directory.sh
@@ -1,12 +1,17 @@
 #! /bin/sh
 # Move home directory to separate data partition
 
+# Unmount home directory partition just in case
 homedir=$(df -h | grep -o "/media/pi/home.*")
 log_daemon_msg "STBDEV: home directory = $homedir"
-cd /home
-sudo cp -rp ./ $homedir
 sudo umount $homedir
-sudo rm -rf $homedir
+
+sudo mkdir /tmp/home
+sudo mount /dev/mmcblk0p8 /tmp/home
+cd /home
+sudo cp -rp ./ /tmp/home
+sudo umount /tmp/home
+sudo rm -rf /tmp/home
 sudo mv /home /old_home
 sudo mkdir /home
 sudo chmod 666 /etc/fstab
