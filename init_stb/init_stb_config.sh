2c967af916266f68b59df7e2c757bf979bf2b9fe
Claus Still <claus.still@gmail.com>
2016-07-30 02:16:33 +0300

    cd /etc/init.d before getting install scripts

--- init_stb/init_stb_config.sh
+++ init_stb/init_stb_config.sh
@@ -5,22 +5,23 @@
 
 set -x
 
-cd /etc/init.d
-
 # Init OS settings
 log_daemon_msg "STBDEV: Initializing OS settings for IOT STB"
+cd /etc/init.d
 sudo wget https://github.com/cstinv/stbdev/raw/master/init_stb/init_os_settings.sh
 chmod 774 init_os_settings.sh
 . /etc/init.d/init_os_settings.sh
 
 # Init home directory on separate partition
 log_daemon_msg "STBDEV: Initializing home directory on separate partition"
+cd /etc/init.d
 sudo wget https://github.com/cstinv/stbdev/raw/master/init_stb/init_home_directory.sh
 chmod 774 init_home_directory.sh
 . /etc/init.d/init_home_directory.sh
 
 # Install tvheadned
 log_daemon_msg "STBDEV: Installing tvheadend on the STB"
+cd /etc/init.d
 sudo wget https://github.com/cstinv/stbdev/raw/master/init_stb/install_tv_headend.sh
 chmod 774 install_tv_headend.sh
 . /etc/init.d/install_tv_headend.sh
