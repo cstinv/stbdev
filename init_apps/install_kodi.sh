6116c5fff10d26b206c275d35b9cbfb6804429b6
Claus Still <claus.still@gmail.com>
2016-08-10 12:01:53 +0300

    Support for installing KODI

---
+++ init_apps/install_kodi.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+# Install kodi player and default addons
+
+. /lib/lsb/init-functions
+
+set -x
+
+sudo apt-get -y update
+sudo apt-get -y install kodi
+
+# Create /etc/udev/rules.d/99-input.rules file
+sudo sh -c "echo 'SUBSYSTEM==input, GROUP=input, MODE=0660' > /etc/udev/rules.d/99-input.rules"
+sudo sh -c "echo 'KERNEL==tty[0-9]*, GROUP=tty, MODE=0660' >> /etc/udev/rules.d/99-input.rules"
+
+# Create /etc/udev/rules.d/10-permissions.rules
+
+sudo sh -c "echo '# input' > /etc/udev/rules.d/10-permissions.rules"
+sudo sh -c "echo 'KERNEL==\"mouse*|mice|event*\",   MODE=\"0660\", GROUP=\"input\"' >> /etc/udev/rules.d/10-permissions.rules"
+sudo sh -c "echo 'KERNEL==\"ts[0-9]*|uinput\",     MODE=\"0660\", GROUP=\"input\"' >> /etc/udev/rules.d/10-permissions.rules"
+sudo sh -c "echo 'KERNEL==js[0-9]*,             MODE=0660, GROUP=input' >> /etc/udev/rules.d/10-permissions.rules"
+sudo sh -c "echo '# tty' >> /etc/udev/rules.d/10-permissions.rules"
+sudo sh -c "echo 'KERNEL==tty[0-9]*,            MODE=0666' >> /etc/udev/rules.d/10-permissions.rules"
+sudo sh -c "echo '# vchiq' >> /etc/udev/rules.d/10-permissions.rules"
+sudo sh -c "echo 'SUBSYSTEM==vchiq,  GROUP=video, MODE=0660' >> /etc/udev/rules.d/10-permissions.rules"
+
+# Additional settings for user pi
+Run the following commands for user pi:
+sudo usermod -a -G audio pi
+sudo usermod -a -G video pi
+sudo usermod -a -G input pi
+sudo usermod -a -G dialout pi
+sudo usermod -a -G plugdev pi
+sudo usermod -a -G tty pi
+
+#Edit /boot/config.txt for Full HD support
+sudo sh -c "echo '  ' >> /boot/config.txt"
+sudo sh -c "echo '# Full HD support' >> /boot/config.txt"
+sudo sh -c "echo 'gpu_mem=160' >> /boot/config.txt"
+
