a1046126c6b8d595a4402f110717843b7bd5c521
Claus Still <claus.still@gmail.com>
2016-08-27 23:14:26 +0300

    cd to home directory before running install

--- init_apps/init_apps.sh
+++ init_apps/init_apps.sh
@@ -15,6 +15,7 @@ sudo chmod 774 install_tv_headend.sh
 
 # Install kodi
 log_daemon_msg "STBDEV: Installing KODI on the STB"
+cd ~
 sudo wget https://github.com/cstinv/stbdev/raw/master/init_apps/install_kodi.sh
 sudo chmod 774 install_kodi.sh
 . ~/install_kodi.sh
