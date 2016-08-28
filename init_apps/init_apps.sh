e3984b025021920929628caeeb966479030d0699
Claus Still <claus.still@gmail.com>
2016-08-28 11:35:21 +0300

    removing sudo from init_apps shell execution

--- init_apps/init_apps.sh
+++ init_apps/init_apps.sh
@@ -5,20 +5,19 @@
 
 set -x
 
-cd /home/pi
-
 # Install tvheadend
 log_daemon_msg "STBDEV: Installing tvheadend on the STB"
+cd /home/pi
 sudo wget https://github.com/cstinv/stbdev/raw/master/init_apps/install_tv_headend.sh
 sudo chmod 774 install_tv_headend.sh
-sudo . /home/pi/install_tv_headend.sh
+. /home/pi/install_tv_headend.sh
 
 # Install kodi
 log_daemon_msg "STBDEV: Installing KODI on the STB"
 cd /home/pi
 sudo wget https://github.com/cstinv/stbdev/raw/master/init_apps/install_kodi.sh
 sudo chmod 774 install_kodi.sh
-sudo . /home/pi/install_kodi.sh
+. /home/pi/install_kodi.sh
 
 
 
