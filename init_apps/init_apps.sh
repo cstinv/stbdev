f8015d3b06d5c863507cab6f99961f2011463645
Claus Still <claus.still@gmail.com>
2016-09-04 11:01:55 +0300

    Added preseeding options to tvheadend

--- init_apps/init_apps.sh
+++ init_apps/init_apps.sh
@@ -8,9 +8,11 @@ set -x
 # Install tvheadend
 log_daemon_msg "STBDEV: Installing tvheadend on the STB"
 cd /home/pi
-sudo wget https://github.com/cstinv/stbdev/raw/master/init_apps/install_tv_headend.sh
-sudo chmod 774 install_tv_headend.sh
-. /home/pi/install_tv_headend.sh
+sudo wget https://github.com/cstinv/stbdev/raw/master/init_apps/install_tvheadend.sh
+sudo chmod 774 install_tvheadend.sh
+sudo wget https://github.com/cstinv/stbdev/raw/master/init_apps/tvheadend.seed
+debconf-set-selections ./tvheadend.seed
+. /home/pi/install_tvheadend.sh
 
 # Install kodi
 log_daemon_msg "STBDEV: Installing KODI on the STB"
