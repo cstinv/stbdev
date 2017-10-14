621c506cb534ba37228c48d95804c1a681e522a7
Claus Still <claus.still@gmail.com>
2017-10-14 14:11:58 +0300

    New version of dvb-fe-scan tool for scanning DVB-T and DVB-T2 channels

--- init_apps/install_dvb_tools.sh
+++ init_apps/install_dvb_tools.sh
@@ -6,8 +6,8 @@
 set -x
 
 # Install DVB tools package
-sudo wget https://github.com/cstinv/stb_pkg/raw/master/v4l-utils_201709280020-1_armhf.deb
-sudo dpkg -i v4l-utils_201709280020-1_armhf.deb
+sudo wget sudo wget "https://drive.google.com/uc?export=download&id=0B0-DhBnH4CTKZVBCZm9vRGZHMGc" -O v4l-utils_201710141338-1_armhf.deb
+sudo dpkg -i v4l-utils_201710141338-1_armhf.deb
 sudo apt-get -y install -f
 
 
