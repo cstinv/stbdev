d4187a9c7b69972723b2118c5587806f5a9cde20
Claus Still <claus.still@gmail.com>
2017-10-15 13:02:03 +0300

    Support for DVB-C in dvb-fe-scan

--- init_apps/install_dvb_tools.sh
+++ init_apps/install_dvb_tools.sh
@@ -6,8 +6,7 @@
 set -x
 
 # Install DVB tools package
-sudo wget sudo wget "https://drive.google.com/uc?export=download&id=0B0-DhBnH4CTKZVBCZm9vRGZHMGc" -O v4l-utils_201710141338-1_armhf.deb
-sudo dpkg -i v4l-utils_201710141338-1_armhf.deb
+sudo wget sudo wget "https://drive.google.com/uc?export=download&id=0B0-DhBnH4CTKR3RlMVFtTFdOZTA" -O v4l-utils_20171015122900-1_armhf.deb
+sudo dpkg -i v4l-utils_20171015122900-1_armhf.deb
 sudo apt-get -y install -f
-
 
